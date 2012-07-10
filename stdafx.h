eaf5d90d2827861269095c770181fa1f89bfabc7
nullcoding@gmail.com <nullcoding@gmail.com@b294614b-1404-64ef-572d-408f59b1fc38>
2012-07-10 15:48:33 +0000

    to be used with gjsieve_tester15 AND arraymaker.

---
+++ stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <math.h>
+#include <dos.h>
+#include <Windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <stdlib.h>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <istream>
+#include <vector>
+#include <array>
+#include <string>
+
+#include "targetver.h"
